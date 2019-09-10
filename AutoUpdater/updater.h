88ccdd07667d4a9774926cf84a2a1de1232c4140
xtcuser <onureser12@gmail.com>
2019-09-10 15:44:59 +0300

    removed special cloud server login config

--- AutoUpdater/updater.h
+++ AutoUpdater/updater.h
@@ -17,9 +17,9 @@ class Updater : public QObject
     QString m_version = "1.0"; //current version
     QString m_next_version;
     //QString m_proto_loc = QString("%1/proto.ini").arg(QDir::currentPath());
-    QString m_update_server = "cloud.kodpazar.com";
-    QString m_username = "admin_dizaynvip2";
-    QString m_password = "HhPhf2FnO3";
+    QString m_update_server = "server.com"; //
+    QString m_username = "username"; //
+    QString m_password = "password"; //
     QString m_updatedir;
     QString m_targetpath;
     QString m_updatepath;
